e2d85e9ead51659930837b60460b09d39b8a5772
EricSal <694748417@qq.com>
2018-03-19 23:21:17 +0800

    对工程一些bug进行修改调调试

--- src/wcbasic.java
+++ src/wcbasic.java
@@ -5,58 +5,83 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.FileOutputStream;
+import java.lang.String;
 
-public class wcbasic {
+public class wcbasic{
+    static BufferedReader br;
+    static FileOutputStream fos;
+    static int countWord=0;
+    static int countChar=0;
+    static int countLine=0;
+    private String filepath = "undefined";
+    public wcbasic(String filepath)throws FileNotFoundException{
+        this.br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(filepath))));
+        this.fos = new FileOutputStream (new File("output.txt"));
+        this.filepath = filepath;
+    }
     public void wcexe(char m) throws IOException{
-        String filepath="F:/test1.txt";//文件路径
-        BufferedReader br =null;
-        int countWord=0;
-        int countChar=0;
-        int countLine=0;
         String s="";
         String strCount="";
         try {
-            br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(filepath))));
             while((s=br.readLine())!=null)
             {
-                s=s+" ";
-                strCount+=s;
+                countChar += s.length()+1;
+                if(s.contains("\t")){
+                    countChar--;
+                }
+                strCount+=(s+" ");
                 countLine++;
             }
-            for(int i=0;i<strCount.split(" ").length;i++){
-                if(!strCount.split(" ")[i].equals(" "))
-                    countWord++;
-                countChar+= strCount.split(" ")[i].length();
-            }
-            File file = new File("F:/output.txt");
-            FileOutputStream fos = new FileOutputStream(file);
+            String results[]  = strCount.split(" |,|\\n|\\t");
+            countWord = results.length;
             String content = null;
             switch(m){
                 case 'c':
-                    content="字符数："+countChar;
+                    content=filepath+" 字符数："+countChar;
+                    System.out.print(filepath+" 字符数："+countChar);
                     break;
                 case 'w':
-                    content="单词数："+countWord;
+                    content=filepath+" 单词数："+countWord;
+                    System.out.print(filepath+" 单词数："+countWord);
                     break;
                 case 'l':
-                    content="行数："+countLine;
+                    content=filepath+" 行数："+countLine;
+                    System.out.print(filepath+" 行数："+countLine);
                     break;
                 case 'o':
-                    content="字符数："+countChar+" 单词数："+countWord+" 行数："+countLine;
+                    content=filepath+" 字符数："+countChar+" 单词数："+countWord+" 行数："+countLine;
+                    System.out.print(filepath+" 字符数："+countChar+" 单词数："+countWord+" 行数："+countLine);
+                    break;
             }
             fos.write(content.getBytes());
-            fos.flush();
-            fos.close();
+
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }finally{
-            br.close();
         }
     }
     public static void main(String[] args) throws IOException{
-        wcbasic w=new wcbasic();
-        char i = (char) System.in.read();
-        w.wcexe(i);
+        char m=' ';
+        String filepath= " ";
+        if(args.length>=2){
+            filepath= args[args.length-1];
+            wcbasic w=new wcbasic(filepath);
+            for (int i = 0; i < args.length-1; i++) {
+                if (args[i].charAt(0) == '-') {
+                    char t = args[i].charAt(1);
+                    if(t == 'w'|| t == 'l'|| t == 'c'||t == 'o'){
+                        w.wcexe(t);
+                    }else{
+                        System.out.println("wrong command");
+                    }
+                } else {
+                    System.out.println("wrong command");
+                }
+            }
+        }
+        wcbasic.fos.flush();
+        wcbasic.fos.close();
+        wcbasic.br.close();
+        }
     }
-}
